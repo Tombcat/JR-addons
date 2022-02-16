c335be2cb0f2ac0b839c12ac8b087b1ed4d8419a
Marcin Włodarczyk <marcin.wlodarczyk98@gmail.com>
2022-02-16 16:10:57 +0100

    Init commit

---
+++ src/server.js
@@ -0,0 +1,108 @@
+import express, { response } from 'express'
+import axios from 'axios';
+import Pool from "pg-pool";
+
+import config from './config/index.js';
+import bodyParser from 'body-parser';
+
+const pool = new Pool({
+  user: "michelle",
+  database: "zencars",
+  password: "JustRent2021!",
+  port: 5432,
+  host: "db-sandbox2.cu6ycwny8ckg.eu-west-1.rds.amazonaws.com",
+});
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const runQuery = async function (client, query) {
+    try {
+      var result = await client.query(query.text, query.values)
+      return result
+    } finally {
+        
+    }
+  }
+
+const scrapeReezo = async function(client, scrape) {
+    for(const el of scrape.data.ads.ads){
+        let query = {
+            text: "SELECT * FROM public.reezocar_scrapes WHERE brand = $1 AND model = $2 AND mileage = $3 AND price = $4 and year = $5",
+            values: [el.brand,el.model,el.mileage,el.price,el.year]
+        }
+    
+        await runQuery(client, query).then(result=>{
+            if (result.rows.length !== 0) continue
+            
+        }).catch(e => {
+            console.error(el, e.message, e.stack)
+        })
+    
+        query = {
+            text: "INSERT INTO reezocar_scrapes (reezo_id, title, brand, model, energy, gearbox, mileage, price, year) \
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+            values: [el._id, el.title, el.brand, el.model, el.energy, el.gearbox, el.mileage, el.price, el.year]
+        }
+        await runQuery(client, query).then(result=>{
+            
+        }).catch(e => {
+            console.error(el, e.message, e.stack)
+        }) 
+        
+    }
+};
+
+const looper = async function(query){
+    var client = await pool.connect()
+    console.log("Client Connected")
+
+    let from = (query.variables.query.from)? query.variables.query.from : 0
+    let result
+
+    do{
+        console.log(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), "Starting from:", from)
+        query.variables.query.from = from
+        result = await fetchReezo(query).then((scrape)=>{
+            return scrape
+        }).catch(error=>{
+            console.log(error)
+        })
+        await scrapeReezo(client, result)
+        from+=100
+    }while(from<result.data.ads.count)
+
+    client.release()
+    console.log("Client disconnected")
+
+    return "Looper is done!"
+}
+
+function fetchReezo(data){
+    return axios.get("https://gqlaws.reezocar.com/graphql", {
+        data: data
+    }).then(response=>{
+        return response.data
+    }).catch(error=>{
+        console.log ("------------------- Reezo scrape error -------------------")
+        throw new Error(error);
+    })
+}
+
+app.post('/test', (req, res) => {
+    res.status(202).send("Accepted, in progress!")
+    looper(req.body).then((response)=>{
+        console.log(response)
+    }).catch(error=>{
+        console.log(error)
+    })
+})
+
+app.get('/', (req,res)=>{
+    res.send("Working")
+})
+
+app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`)
+})
