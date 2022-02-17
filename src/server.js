c172b1f3db4f79a918545cdabf6039c46368b460
Marcin Włodarczyk <marcin.wlodarczyk98@gmail.com>
2022-02-17 16:03:25 +0100

    Make scrapeReezo Try

--- src/server.js
+++ src/server.js
@@ -23,6 +23,8 @@ const runQuery = async function (client, query) {
     try {
       var result = await client.query(query.text, query.values)
       return result
+    } catch (e) {
+        throw new Error('error while fetching query');
     } finally {
         
     }
@@ -30,30 +32,27 @@ const runQuery = async function (client, query) {
 
 const scrapeReezo = async function(client, scrape) {
     for(const el of scrape.data.ads.ads){
-        let query = {
-            text: "SELECT * FROM public.reezocar_scrapes WHERE brand = $1 AND model = $2 AND mileage = $3 AND price = $4 and year = $5",
-            values: [el.brand,el.model,el.mileage,el.price,el.year]
-        }
-    
-        const length = await runQuery(client, query).then(result=>{
-            return result.rows.length
-        }).catch(e => {
-            console.error(el, e.message, e.stack)
-        })
-
-        if(length !== 0) continue
-    
-        query = {
-            text: "INSERT INTO reezocar_scrapes (reezo_id, title, brand, model, energy, gearbox, mileage, price, year) \
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
-            values: [el._id, el.title, el.brand, el.model, el.energy, el.gearbox, el.mileage, el.price, el.year]
-        }
-        await runQuery(client, query).then(result=>{
-            
-        }).catch(e => {
-            console.error(el, e.message, e.stack)
-        }) 
+        try {
+            let query = {
+                text: "SELECT * FROM public.reezocar_scrapes WHERE brand = $1 AND model = $2 AND mileage = $3 AND price = $4 and year = $5",
+                values: [el.brand,el.model,el.mileage,el.price,el.year]
+            }
         
+            const length = await runQuery(client, query)
+
+            if(length !== 0) continue
+        
+            query = {
+                text: "INSERT INTO reezocar_scrapes (reezo_id, title, brand, model, energy, gearbox, mileage, price, year) \
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+                values: [el._id, el.title, el.brand, el.model, el.energy, el.gearbox, el.mileage, el.price, el.year]
+            }
+            await runQuery(client, query)
+
+        } catch (e){
+            console.log(e.message," \ ", e.stack)
+            throw new Error("Test")
+        }
     }
 };
 
@@ -67,11 +66,7 @@ const looper = async function(query){
     do{
         console.log(new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), "Starting from:", from)
         query.variables.query.from = from
-        result = await fetchReezo(query).then((scrape)=>{
-            return scrape
-        }).catch(error=>{
-            console.log(error)
-        })
+        result = await fetchReezo(query)
         await scrapeReezo(client, result)
         from+=100
     }while(from<result.data.ads.count)
